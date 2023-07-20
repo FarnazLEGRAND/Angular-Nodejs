0c4ce358d7c576b3af8d1275dbf879eeac3640f9
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-07-20 16:48:22 +0200

    liste de person

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Person } from '../entities';
+import { PersonService } from '../person.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  list:Person[] = [];
+  // select mikone yek adam ra ye 
+  selected:Person|null = null;
+  constructor(private personService:PersonService){}
+
+  ngOnInit(): void {
+    
+    this.personService.fetchAll().subscribe(data => this.list = data);
+    
+  }
 
 }
